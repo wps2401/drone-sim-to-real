c07e1618beffea259f42fd6b411937e8dda82948
Will Schuhmann <wps2401@gmail.com>
2026-06-21 17:30:52 -0500

    Rest of physics

--- sim1/physics.py
+++ sim1/physics.py
@@ -22,8 +22,26 @@ class DronePhysics:
         }
 
     def step(self, thrust, roll_torque, pitch_torque, yaw_torque):
+
         inertia = 0.01
+        mass = 1.0
+        gravity = 9.81
+        ax = (thrust / mass) * np.sin(self.state['pitch'])
+        ay = (thrust / mass) * -np.sin(self.state['roll'])
+        az = (thrust / mass) * np.cos(self.state['roll']) * np.cos(self.state['pitch']) - gravity
+        
         self.state['p'] += (roll_torque / inertia) * self.dt
         self.state['q'] += (pitch_torque / inertia) * self.dt
         self.state['r'] += (yaw_torque / inertia) * self.dt
+        self.state['roll'] += self.state['p'] * self.dt
+        self.state['pitch'] += self.state['q'] * self.dt
+        self.state['yaw'] += self.state['r'] * self.dt
+
+        self.state['vx'] += ax * self.dt
+        self.state['vy'] += ay * self.dt
+        self.state['vz'] += az * self.dt
+        self.state['x'] += self.state['vx'] * self.dt
+        self.state['y'] += self.state['vy'] * self.dt
+        self.state['z'] += self.state['vz'] * self.dt
+
         return self.state
